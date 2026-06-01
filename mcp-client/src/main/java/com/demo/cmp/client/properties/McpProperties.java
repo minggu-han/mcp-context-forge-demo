f2b97696df220c2459efdbd40f2fa847abf75a08
snowhan <309859406@qq.com>
2026-06-01 21:18:30 +0800

    docker compose

---
+++ mcp-client/src/main/java/com/demo/cmp/client/properties/McpProperties.java
@@ -0,0 +1,16 @@
+package com.demo.cmp.client.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "mcp")
+public class McpProperties {
+
+    private String url;
+
+    private String jwt;
+
+}
